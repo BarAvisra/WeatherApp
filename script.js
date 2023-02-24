099ed69ca5f5a6e07a4b21ffbeaa58d3745455f4
BarAvisra <cripsy3@gmail.com>
2023-02-25 00:37:05 +0200

    added HTML upper container

---
+++ script.js
@@ -0,0 +1,27 @@
+const btn = document.querySelector("#btn1");
+const result = document.querySelector("#result");
+
+const apiKey = "309686f5d1b5556701ea19ed878a2fa1"
+
+async function getWeatherData(city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        if (data.cod == "404") {
+            result.innerHTML = "Country cannot be found!";
+        }
+        else {
+            result.innerHTML = `The temperature in ${city} is ${data.main.temp}°C`;
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+btn.addEventListener("click", () => {
+    const input = document.querySelector("#input1").value;
+    getWeatherData(input);
+})
