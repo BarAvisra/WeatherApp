88e0b2ba26fa8b46a1ed5555f3e2d56ab940c964
BarAvisra <cripsy3@gmail.com>
2023-04-07 18:27:48 +0300

    Minor changes in HTML,CSS, and big update in JS

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const searchButton = document.querySelector("#searchIcon");
 const cityCountry = document.querySelector(".city_country");
 const temperature = document.querySelector(".temp");
 const weatherDescription = document.querySelector(".weather_description");
+const weatherImage = document.querySelector(".weather_icon");
+const windIndicator = document.querySelector("#wind_indicator");
+const humidityIndicator = document.querySelector("#humidity_indicator");
+const maxTempIndicator = document.querySelector("#max_temp_indicator");
+const middleUpperDiv = document.querySelector(".middle-upper")
 
 const apiKey = "309686f5d1b5556701ea19ed878a2fa1";
 
@@ -10,15 +15,21 @@ async function getWeatherData(city) {
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
-
+    console.log(data);
     if (data.cod == "404") {
       cityCountry.innerHTML = "location not found";
     } else {
       cityCountry.innerHTML = data.name;
       const integerTemp = Math.round(data.main.temp);
-      temperature.innerHTML = `${integerTemp}°C`;
+
+      const celsiusSymbol = document.createElement('span');
+      celsiusSymbol.innerHTML = '°C';
+      celsiusSymbol.classList.add('celsius');
+
+      temperature.innerHTML = `${integerTemp}`;
       weatherDescription.innerHTML = data.weather[0].description;
-      console.log(weatherDescription);
+
+      temperature.append(celsiusSymbol)
     }
 
     const sunrise = data.sys.sunrise;
@@ -27,6 +38,7 @@ async function getWeatherData(city) {
     const currentTime = new Date().getTime() / 1000;
 
     const weatherConditions = data.weather[0].main;
+    console.log(weatherConditions);
     const isRaining = weatherConditions === "Rain";
     const isSunny = weatherConditions === "Clear";
     const isCloudy = weatherConditions === "Clouds";
@@ -34,65 +46,67 @@ async function getWeatherData(city) {
     const isHaze = weatherConditions === "Haze";
     const isDrizzle = weatherConditions === "Drizzle";
 
-    console.log(weatherConditions);
-
-    let image;
-
     switch (true) {
       case isRaining && currentTime > sunrise && currentTime < sunset:
         // it's daytime and raining
-        image = "day-raining.png";
+        weatherImage.src = "../icons/weather-icons/day-raining.svg";
         break;
       case isSunny && currentTime > sunrise && currentTime < sunset:
         // it's daytime and sunny
-        image = "day-clear.png";
+        weatherImage.src = "../icons/weather-icons/day-clear.svg";
         break;
       case isCloudy && currentTime > sunrise && currentTime < sunset:
         // it's daytime and cloudy
-        image = "day-cloudy.png";
+        weatherImage.src = "../icons/weather-icons/day-cloudy.svg";
         break;
       case isSnowing && currentTime > sunrise && currentTime < sunset:
         // it's daytime and snowing
-        image = "day-snowing.png";
+        weatherImage.src = "../icons/weather-icons/day-snowing.svg";
         break;
       case isHaze && currentTime > sunrise && currentTime < sunset:
         // it's daytime and hazy
-        image = "day-haze.png";
+        weatherImage.src = "../icons/weather-icons/day-clear.svg";
         break;
       case isDrizzle && currentTime > sunrise && currentTime < sunset:
         // it's daytime and drizzle
-        image = "day-drizzle.png";
+        weatherImage.src = "../icons/weather-icons/day-raining.svg";
         break;
 
       case isRaining && currentTime > sunset:
         // it's nighttime and raining
-        image = "night-raining.png";
+        weatherImage.src = "../icons/weather-icons/night-raining.svg";
         break;
       case isSunny && currentTime > sunset:
         // it's nighttime and sunny
-        image = "night-clear.png";
+        weatherImage.src = "../icons/weather-icons/night-clear.svg";
         break;
       case isCloudy && currentTime > sunset:
         // it's nighttime and cloudy
-        image = "night-cloudy.png";
+        weatherImage.src = "../icons/weather-icons/night-cloudy.svg";
         break;
       case isSnowing && currentTime > sunset:
         // it's nighttime and snowing
-        image = "night-snowing.png";
+        weatherImage.src = "../icons/weather-icons/night-snowing.svg";
         break;
       case isHaze && currentTime > sunset:
         // it's nighttime and hazy
-        image = "night-haze.png";
+        weatherImage.src = "../icons/weather-icons/night-clear.svg";
         break;
       case isDrizzle && currentTime > sunset:
         // it's nighttime and drizzle
-        image = "night-drizzle.png";
+        weatherImage.src = "../icons/weather-icons/night-raining.svg";
         break;
       default:
         // default image
         image = "";
         break;
     }
+
+    windIndicator.innerHTML = data.wind.speed;
+    humidityIndicator.innerHTML = data.main.humidity;
+    maxTempIndicator.innerHTML = Math.round(data.main.temp_max);
+
+
   } catch (error) {
     console.log(error);
   }
